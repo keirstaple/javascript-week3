2c59fc87ca70d6afe70ea65e791f1fb58e3ee22d
keir staple <keirstaple@comcast.net>
2016-04-01 14:12:13 -0700

    add answer handlebars

--- app/models/question.js
+++ app/models/question.js
@@ -4,5 +4,5 @@ export default DS.Model.extend({
   text: DS.attr(),
   author: DS.attr(),
   notes: DS.attr()
-  // answers: DS.hasMany('answer', {async: true})
+  answers: DS.hasMany('answer', {async: true})
 });
