72cd0f79da21ea1f37a65be569081635e59fc63d
keir staple <keirstaple@comcast.net>
2016-04-01 11:41:20 -0700

    add ability to ask a new question

---
+++ app/components/new-question.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+
+    save() {
+      var params = {
+        text: this.get('text'),
+        author: this.get('author'),
+        notes: this.get('notes'),
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('save', params);
+    }
+  }
+});
