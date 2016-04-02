b25a3728eceeae16ced5dc7f3ff8a525ae0bb1c4
keir staple <keirstaple@comcast.net>
2016-04-01 17:19:11 -0700

    add styles to navbar--much more needed

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -4,6 +4,12 @@ export default Ember.Component.extend({
   actions: {
     saveAnswer(answer, params) {
       this.sendAction('saveAnswer', answer, params);
-    }
+    },
+
+    // delete(answer) {
+    //   if (confirm("Are you sure you want to delete your answer?")) {
+    //     this.sendAction('destroyAnswer', answer);
+    //   }
+    // }
   }
 });
--- app/routes/answer.js
+++ app/routes/answer.js
@@ -4,4 +4,11 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('answer', params.answer_id);
   }
+
+  // actions: {
+  //   destroyAnswer(answer) {
+  //     answer.destroyRecord();
+  //     this.transitionTo('index');
+  //   }
+  // }
 });
